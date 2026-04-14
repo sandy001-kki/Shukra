7e61cee60fe5e37386ba39860aef41ad18ae0693
Bollavaram Sandeep Kumar <bollavaramsandeep@gmail.com>
2026-04-14 23:21:51 +0530

    Remove unused CLI helper for lint clean

--- pkg/cli/helpers.go
+++ pkg/cli/helpers.go
@@ -4,12 +4,7 @@ package cli
 
 import (
 	networkingv1 "k8s.io/api/networking/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 )
-
-func intstrFromInt32(value int32) intstr.IntOrString {
-	return intstr.FromInt(int(value))
-}
 
 func networkingPathType() networkingv1.PathType {
 	return networkingv1.PathTypePrefix
